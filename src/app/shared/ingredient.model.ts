67077019ab6ec0bc337ad86c0caf0086fee46bb3
manojk08 <kalemanoj3@gmail.com>
2020-07-28 16:45:50 +0530

    update app

--- src/app/shared/ingredient.model.ts
+++ src/app/shared/ingredient.model.ts
@@ -1,5 +1,3 @@
 export class Ingredient {
-  public name: string;
-  public amount: number;
-  constructor(name: string, amount: number) {}
+  constructor(public name: string, public amount: number) {}
 }
