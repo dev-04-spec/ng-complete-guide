0d3ab88e974db0af898172a656b5179f3a6a1bf1
manojk08 <kalemanoj3@gmail.com>
2020-08-02 00:51:40 +0530

    Managing Recipes in a Recipe service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,29 +1,18 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe(
-      'Chicken piece fry',
-      'Delicious',
-      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-crispy-chicken-thighs-garlic-rosemary-1567793052.png?crop=0.670xw:1.00xh;0.0513xw,0&resize=640:*'
-    ),
-    new Recipe(
-      'Chicken Curry',
-      'Delicious',
-      'https://i.ndtvimg.com/i/2017-12/malvani-chicken-curry_620x330_81514354104.jpg'
-    ),
-    new Recipe(
-      'Mutton Curry',
-      'Delicious',
-      'https://www.indianhealthyrecipes.com/wp-content/uploads/2016/05/mutton-curry-1-500x500.jpg'
-    ),
-  ];
+  recipes: Recipe[];
+  constructor(private recipeService: RecipeService) {}
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
+  }
   onRecipeSelected(recipe: Recipe) {
     this.recipeWasSelected.emit(recipe);
   }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,1 +1,23 @@
-export class RecipeService {}
+import { Recipe } from './recipe.model';
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Chicken piece fry',
+      'Delicious',
+      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-crispy-chicken-thighs-garlic-rosemary-1567793052.png?crop=0.670xw:1.00xh;0.0513xw,0&resize=640:*'
+    ),
+    new Recipe(
+      'Chicken Curry',
+      'Delicious',
+      'https://i.ndtvimg.com/i/2017-12/malvani-chicken-curry_620x330_81514354104.jpg'
+    ),
+    new Recipe(
+      'Mutton Curry',
+      'Delicious',
+      'https://www.indianhealthyrecipes.com/wp-content/uploads/2016/05/mutton-curry-1-500x500.jpg'
+    ),
+  ];
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
