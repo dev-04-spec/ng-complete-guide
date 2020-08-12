22586ae1eece9a0e827971284f16dbaa83ff6641
manojk08 <kalemanoj3@gmail.com>
2020-08-13 02:24:21 +0530

    adding the Token to outgoing Requests

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,22 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError, tap } from 'rxjs/operators';
-import { throwError, Subject } from 'rxjs';
+import { throwError, BehaviorSubject } from 'rxjs';
+
 import { User } from './user.model';
+
 export interface AuthResponseData {
-  idToken: string; //A Firebase Auth ID token for the newly created user.
-  email: string; //The email for the newly created user.
-  refreshToken: string; //	A Firebase Auth refresh token for the newly created user.
-  expiresIn: string; //	The number of seconds in which the ID token expires.
-  localId: string; //	The uid of the newly created user.
-  registered?: boolean; //	Whether the email is for an existing account.
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
 }
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
 export class AuthService {
-  user = new Subject<User>();
+  user = new BehaviorSubject<User>(null);
+
   constructor(private http: HttpClient) {}
 
   signup(email: string, password: string) {
@@ -35,16 +37,17 @@ export class AuthService {
           this.handleAuthentication(
             resData.email,
             resData.localId,
-            resData.refreshToken,
+            resData.idToken,
             +resData.expiresIn
           );
         })
       );
   }
+
   login(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCgfGuqv2WNPcjFyBgfzxcD5u-6o37klDo',
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCgfGuqv2WNPcjFyBgfzxcD5u-6o37klDo',
         {
           email: email,
           password: password,
@@ -57,7 +60,7 @@ export class AuthService {
           this.handleAuthentication(
             resData.email,
             resData.localId,
-            resData.refreshToken,
+            resData.idToken,
             +resData.expiresIn
           );
         })
@@ -66,70 +69,30 @@ export class AuthService {
 
   private handleAuthentication(
     email: string,
-    localId: string,
-    refreshToken: string,
+    userId: string,
+    token: string,
     expiresIn: number
   ) {
-    const expirationDate = new Date(new Date().getTime() + +expiresIn * 1000);
-    const user = new User(email, localId, refreshToken, expirationDate);
-
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(email, userId, token, expirationDate);
     this.user.next(user);
   }
+
   private handleError(errorRes: HttpErrorResponse) {
-    let errorMessage = 'An unknown error message occurred';
+    let errorMessage = 'An unknown error occurred!';
     if (!errorRes.error || !errorRes.error.error) {
       return throwError(errorMessage);
     }
     switch (errorRes.error.error.message) {
       case 'EMAIL_EXISTS':
-        errorMessage = 'The email address is already in use by another account';
-        break;
-      case 'OPERATION_NOT_ALLOWED':
-        errorMessage = 'Password sign-in is disabled for this project';
-        break;
-      case 'TOO_MANY_ATTEMPTS_TRY_LATER':
-        errorMessage =
-          'We have blocked all requests from this device due to unusual activity. Try again later.';
-        break;
-      case 'OPERATION_NOT_ALLOWED':
-        errorMessage = 'Password sign-in is disabled for this project.';
-        break;
-      case 'EXPIRED_OOB_CODE':
-        errorMessage = 'The action code has expired.';
-        break;
-      case 'INVALID_OOB_CODE':
-        errorMessage =
-          'The action code is invalid. This can happen if the code is malformed, expired, or has already been used.';
-        break;
-      case 'USER_DISABLED':
-        errorMessage =
-          'The user account has been disabled by an administrator.';
-        break;
-      case 'INVALID_ID_TOKEN':
-        errorMessage =
-          "The user's credential is no longer valid. The user must sign in again.";
-        break;
-      case 'INVALID_ID_TOKEN':
-        errorMessage =
-          "The user's credential is no longer valid. The user must sign in again.";
-        break;
-      case 'WEAK_PASSWORD':
-        errorMessage = 'The password must be 6 characters long or more.';
+        errorMessage = 'This email exists already';
         break;
       case 'EMAIL_NOT_FOUND':
-        errorMessage =
-          'There is no user record corresponding to this identifier. The user may have been deleted.';
+        errorMessage = 'This email does not exist.';
         break;
       case 'INVALID_PASSWORD':
-        errorMessage =
-          'The password is invalid or the user does not have a password.';
+        errorMessage = 'This password is not correct.';
         break;
-      case 'USER_DISABLED':
-        errorMessage =
-          'The user account has been disabled by an administrator.';
-        break;
-      default:
-        errorMessage;
     }
     return throwError(errorMessage);
   }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,41 +1,53 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { RecipeService } from '../recipes/recipe.service';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { map, tap, take, exhaustMap } from 'rxjs/operators';
+
 import { Recipe } from '../recipes/recipe.model';
-import { map ,tap} from 'rxjs/operators';
-@Injectable({
-  providedIn: 'root',
-})
+import { RecipeService } from '../recipes/recipe.service';
+import { AuthService } from '../auth/auth.service';
+
+@Injectable({ providedIn: 'root' })
 export class DataStorageService {
-  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService,
+    private authService: AuthService
+  ) {}
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
-    return this.http
-      .put('https://e-shopping-ng.firebaseio.com/recipes.json', recipes)
-      .subscribe((response) => {
+    this.http
+      .put(
+        'https://e-shopping-ng.firebaseio.com/recipes.json',
+        recipes
+      )
+      .subscribe(response => {
         console.log(response);
       });
   }
-  fetchRecipes() {
-    return this.http
-      .get<Recipe[]>('https://e-shopping-ng.firebaseio.com/recipes.json')
-      .pipe(
-        map((recipes) => {
-          //this map is rxjs operator
-          return recipes.map((recipe) => { // this map is javascript keyword
-            return {
-              ...recipe,
-              ingredients: recipe.ingredients ? recipe.ingredients : []
-            };
-          });
-        }),tap(recipes=>{
 
-          this.recipeService.setRecipes(recipes);
-        })
-      )
-      // .subscribe((recipes) => {
-      //   console.log(recipes);
-      // });
+  fetchRecipes() {
+    return this.authService.user.pipe(
+      take(1),
+      exhaustMap(user => {
+        return this.http.get<Recipe[]>(
+          'https://e-shopping-ng.firebaseio.com/recipes.json',
+          {
+            params: new HttpParams().set('auth', user.token)
+          }
+        );
+      }),
+      map(recipes => {
+        return recipes.map(recipe => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : []
+          };
+        });
+      }),
+      tap(recipes => {
+        this.recipeService.setRecipes(recipes);
+      })
+    );
   }
 }
