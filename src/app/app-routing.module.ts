ca3a1b21dabb160f7529fde4d4c0ba5336c74f32
manojk08 <kalemanoj3@gmail.com>
2020-08-15 21:09:25 +0530

    Spliting Modules and Adding Routes to Feature Modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule} from '@angular/router';
 
-import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipeResolverService } from './recipes/recipe-resolver.service';
 import { AuthComponent } from './auth/auth.component';
 import { AuthGuard } from './auth/auth.guard';
 
@@ -17,24 +12,7 @@ const appRoutes: Routes = [
     pathMatch: 'full',
     canActivate: [AuthGuard],
   },
-  {
-    path: 'recipes',
-    component: RecipesComponent,
-    children: [
-      { path: '', component: RecipeStartComponent ,canActivate: [AuthGuard]},
-      { path: 'new', component: RecipeEditComponent ,canActivate: [AuthGuard]},
-      {
-        path: ':id',
-        component: RecipeDetailComponent,
-        resolve: [RecipeResolverService],canActivate: [AuthGuard]
-      },
-      {
-        path: ':id/edit',
-        component: RecipeEditComponent,
-        resolve: [RecipeResolverService],canActivate: [AuthGuard]
-      },
-    ],
-  },
+
   { path: 'shopping-list', component: ShoppingListComponent },
   { path: 'auth', component: AuthComponent },
 ];
---
+++ src/app/recipes/recipes-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { Routes,RouterModule } from '@angular/router';
+
+import { RecipesComponent } from './recipes.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { AuthGuard } from '../auth/auth.guard';
+import { RecipeResolverService } from '../recipes/recipe-resolver.service';
+const routes: Routes = [
+  {
+    path: 'recipes',
+    component: RecipesComponent,
+    children: [
+      { path: '', component: RecipeStartComponent, canActivate: [AuthGuard] },
+      { path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard] },
+      {
+        path: ':id',
+        component: RecipeDetailComponent,
+        resolve: [RecipeResolverService],
+        canActivate: [AuthGuard],
+      },
+      {
+        path: ':id/edit',
+        component: RecipeEditComponent,
+        resolve: [RecipeResolverService],
+        canActivate: [AuthGuard],
+      },
+    ],
+  },
+];
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+
+})
+export class RecipesRoutingModule {}
