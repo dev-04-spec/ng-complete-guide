dd684211f3e49e2c38ee2df0a618f5ec4d4e51d0
manojk08 <kalemanoj3@gmail.com>
2020-08-15 18:35:05 +0530

    Data Binding and Event Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,8 @@ import { RecipeService } from './recipes/recipe.service';
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { AlertComponent } from './shared/alert/alert.component';
+import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 @NgModule({
   declarations: [
     AppComponent,
@@ -35,6 +37,8 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
     RecipeEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
+    AlertComponent,
+    PlaceholderDirective
   ],
   imports: [
     BrowserModule,
@@ -53,5 +57,6 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
     },
   ],
   bootstrap: [AppComponent],
+  entryComponents:[AlertComponent]
 })
 export class AppModule {}
